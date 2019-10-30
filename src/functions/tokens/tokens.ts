e02f1fb4459555c9f8f1a3c3d7d6a9d9da08f3f4
Charles McCarthy <charles@studybundles.com>
2019-10-30 21:50:19 +0000

    Removed Tokens Endpoint

--- src/functions/tokens/tokens.ts
+++
@@ -1,5 +0,0 @@
-import { TokensController } from './tokens.controller';
-
-const controller: TokensController = new TokensController();
-
-export const storeLoginTokensHandler = controller.storeLoginTokens;
